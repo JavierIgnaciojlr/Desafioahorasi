aaeea483ad8af8a823c50332ec36d6c63bfe926a
JavierIgnaciojlr <lopez.rivas.javier@gmail.com>
2022-03-31 21:47:20 -0300

    fin calculo notas

---
+++ calculo_notas.rb
@@ -0,0 +1,24 @@
+#Debemos crear un metodo nota_mas_alta que reciba un arreglo con el nombre y notas del alumno y devuelva la nota más alta.
+data = open('notas.data').readlines
+data = data.map do |reg| 
+    reg.split(',')
+end
+def nota_mas_alta(arr)
+    notas = arr[1..5]
+
+    notas = notas.map do |note|
+        note.to_i
+    end 
+    notas.max
+end
+
+print nota_mas_alta(data[0]) 
+
+
+
+
+
+
+#def nota_mas_alta(arr)
+#    while split
+#end
